4dc9456cee68eb59d3cbc5bf8515b4d55450b4f4
Sebastian Achim Mueller <sebmuell@phys.ethz.ch>
2019-10-07 11:54:13 +0200

    use the setuptools, as it is currently demoed in pypi

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
     name='sun_grid_engine_map',
-    version='0.0.2',
-    description='Map and reduce for qsub.',
-    url='https://github.com/cherenkov-plenoscope/sun_grid_engine_map',
+    version="0.0.2",
     author='Sebastian Achim Mueller',
     author_email='sebastian-achim.mueller@mpi-hd.mpg.de',
-    license='MIT',
-    packages=[
-        'sun_grid_engine_map',
-    ],
+    description='Map and reduce for qsub.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cherenkov-plenoscope/sun_grid_engine_map',
+    packages=setuptools.find_packages(),
     install_requires=[
+        'xmltodict',
         'qstat',
     ],
-    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Natural Language :: English",
+        "Topic :: System :: Distributed Computing",
+    ],
+    python_requires='>=3',
 )
